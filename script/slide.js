8fd1323d5a4052a3977da059d8a5d6c167e6fe03
Plinio Gonçalves <plinioricardogoncalves@gmail.com>
2023-07-06 05:39:23 -0300

    primeira versao finalizado

--- script/slide.js
+++ script/slide.js
@@ -1,45 +1,45 @@
 window.addEventListener('load', function() {
-    var slides = document.querySelectorAll('#slideshow img');
-    var currentSlide = 0;
-    var slideInterval;
-  
-    var prevButton = document.querySelector('#slideshow .prev');
-    var nextButton = document.querySelector('#slideshow .next');
-  
-    prevButton.addEventListener('click', prevSlide);
-    nextButton.addEventListener('click', nextSlide);
-  
-    function startSlideShow() {
-      slideInterval = setInterval(nextSlide, 10000);
-    }
-  
-    function stopSlideShow() {
-      clearInterval(slideInterval);
-    }
-  
-    function showSlide(slideIndex) {
-      for (var i = 0; i < slides.length; i++) {
-        slides[i].style.display = 'none';
-      }
-      slides[slideIndex].style.display = 'block';
-    }
-  
-    function prevSlide() {
-      slides[currentSlide].style.display = 'none';
-      currentSlide = (currentSlide - 1 + slides.length) % slides.length;
-      showSlide(currentSlide);
-      stopSlideShow();
-      startSlideShow();
-    }
-  
-    function nextSlide() {
-      slides[currentSlide].style.display = 'none';
-      currentSlide = (currentSlide + 1) % slides.length;
-      showSlide(currentSlide);
-      stopSlideShow();
-      startSlideShow();
-    }
-  
-    showSlide(currentSlide);
-    startSlideShow();
+  var slides = document.querySelectorAll('#slideshow img');
+  var slideDetails = document.querySelectorAll('#slideshow .slide-details');
+  var currentSlide = 0;
+  var slideInterval = setInterval(nextSlide, 10000);
+
+  var prevButton = document.querySelector('#slideshow .prev');
+  var nextButton = document.querySelector('#slideshow .next');
+
+  prevButton.addEventListener('click', function() {
+    prevSlide();
+    resetSlideInterval();
   });
+
+  nextButton.addEventListener('click', function() {
+    nextSlide();
+    resetSlideInterval();
+  });
+
+  function showSlide(slideIndex) {
+    for (var i = 0; i < slides.length; i++) {
+      slides[i].style.display = 'none';
+      slideDetails[i].style.display = 'none';
+    }
+    slides[slideIndex].style.display = 'block';
+    slideDetails[slideIndex].style.display = 'block';
+  }
+
+  function prevSlide() {
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    showSlide(currentSlide);
+  }
+
+  function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+  }
+
+  function resetSlideInterval() {
+    clearInterval(slideInterval);
+    slideInterval = setInterval(nextSlide, 10000);
+  }
+
+  showSlide(currentSlide);
+});
