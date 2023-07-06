fd3fba7f7fff418a7aabd6edba4e1a5050467079
Plinio Gonçalves <plinioricardogoncalves@gmail.com>
2023-07-06 04:00:35 -0300

    menu finalizado

--- script/menu.js
+++ script/menu.js
@@ -11,3 +11,13 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   tener('scroll', function () {
     atualizarMenuAtivo();
 });
+
+function highlightMenuItem(event) {
+  const menuItems = document.querySelectorAll('.ul-header li');
+  menuItems.forEach(item => {
+    item.classList.remove('selected');
+  });
+
+  const clickedItem = event.target.parentNode;
+  clickedItem.classList.add('selected');
+}
