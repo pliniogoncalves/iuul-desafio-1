37c683e16cac078c9c2109df5b5deca1cbf16035
Plinio Gonçalves <plinioricardogoncalves@gmail.com>
2023-07-07 00:39:50 -0300

    Armazenamento de Mensagens de Contato

--- script/formulario.js
+++ script/formulario.js
@@ -1,26 +1,45 @@
-    document.querySelector('form').addEventListener('submit', function(event) {
-    event.preventDefault();
+document.querySelector('form').addEventListener('submit', function(event) {
+  event.preventDefault();
 
-    var nameInput = document.getElementById('name');
-    var emailInput = document.getElementById('email');
-    var messageInput = document.getElementById('message');
+  // Obtém os valores dos campos do formulário
+  var nameInput = document.getElementById('name');
+  var emailInput = document.getElementById('email');
+  var messageInput = document.getElementById('message');
 
-    if (nameInput.value === '' || emailInput.value === '' || messageInput.value === '') {
-      var errorMessage = document.createElement('p');
-      errorMessage.textContent = 'Por favor, preencha todos os campos.';
-      errorMessage.style.color = 'red';
-      document.querySelector('form').appendChild(errorMessage);
+  if (nameInput.value === '' || emailInput.value === '' || messageInput.value === '') {
+    var errorMessage = document.createElement('p');
+    errorMessage.textContent = 'Por favor, preencha todos os campos.';
+    errorMessage.style.color = 'red';
+    document.querySelector('form').appendChild(errorMessage);
 
-      return;
-    }
+    return;
+  }
 
-    var successMessage = document.createElement('p');
-    successMessage.textContent = 'E-mail enviado';
-    successMessage.style.color = 'green';
-    document.querySelector('form').appendChild(successMessage);
+  // Cria um objeto com os dados do formulário
+  var formData = {
+    name: nameInput.value,
+    email: emailInput.value,
+    message: messageInput.value
+  };
 
-    nameInput.value = '';
-    emailInput.value = '';
-    messageInput.value = '';
-  });
+  // Obtém as mensagens armazenadas no Local Storage ou cria um novo array vazio
+  var messages = JSON.parse(localStorage.getItem('messages')) || [];
+
+  // Adiciona o objeto do formulário ao array de mensagens
+  messages.push(formData);
+
+  // Converte o array de mensagens para JSON e o armazena no Local Storage
+  localStorage.setItem('messages', JSON.stringify(messages));
+
+  var successMessage = document.createElement('p');
+
+  // Limpa os campos do formulário
+  successMessage.textContent = 'E-mail enviado';
+  successMessage.style.color = 'green';
+  document.querySelector('form').appendChild(successMessage);
+
+  nameInput.value = '';
+  emailInput.value = '';
+  messageInput.value = '';
+});
 
